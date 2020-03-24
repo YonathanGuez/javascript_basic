7b879fd9070ae27cec9618d5c82d56e5a0f60c1a
YO-G <yonath-g@hotmail.fr>
2020-03-24 10:36:18 +0200

    add database into app

--- express_first_app/routes/api/members.js
+++ express_first_app/routes/api/members.js
@@ -7,6 +7,37 @@ router.get('/',(req,res)=>{
     res.json(members);
 });
 
+// Api connection DB
+const db = require('../../db')
+router.get('/user/:id', (req, res, next) => {
+  db.query('SELECT * FROM company WHERE id = $1', [req.params.id], (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.send(result.rows[0].name)
+  });
+});
+
+router.get('/alluser', (req,res, next) => {
+    db.query('SELECT * FROM company', (err, result) => {
+      if (err) {
+        return next(err)
+      }
+      res.send(result.rows);
+    });
+  });
+
+router.post('/username', (req, res, next) => {
+    db.query('SELECT * FROM company WHERE name = $1', [req.body.name], (err, result) => {
+      if (err) {
+        return next(err)
+      }
+      res.json(result.rows[0].id)
+    });
+});
+  
+
+//// Part Without DB 
 // Get singel member
 // http://localhost:3000/api/members/2 => Postman 
 router.get('/:id',(req,res)=>{
@@ -18,6 +49,7 @@ router.get('/:id',(req,res)=>{
         res.status(404).json({msg:`Member not found`});
     }
 });
+
 
 // Create Member 
 router.post('/',(req ,res)=>{
